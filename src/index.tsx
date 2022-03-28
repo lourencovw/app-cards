42db2ec034e8409d1e4f3fdc4263157a2fbe1cde
Lourenco Werneck <lourencovw@gmail.com>
2022-03-28 11:04:41 -0300

    Add routes

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cards from './pages/Cards';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="cards" element={<Cards />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
