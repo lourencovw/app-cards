989579a850469b45b4937fecf23fb2ce9e97fd0a
Lourenco Werneck <lourencovw@gmail.com>
2022-03-28 13:05:41 -0300

    Fix value

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,15 +1,13 @@
 import './Card.css';
 
 function Card(props) {
-  const MAX_VALUE = 10;
-  const value = Math.floor(Math.random() * MAX_VALUE) + 1;
 
   return (
     <div className="card">
       <div>Name: {props.englishName}</div>
       <img className='card-image' src={props.image} alt="" />
       <div>Description: {props.description}</div>
-      <div className='value' >Value: {value}</div>
+      <div className='value' >Value: {props.value}</div>
     </div>
   );
 }
