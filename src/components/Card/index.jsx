1d1ba2eca3b84ac407a2eddcefa93209b32b45ec
Lourenco Werneck <lourencovw@gmail.com>
2022-03-28 11:11:49 -0300

    Add card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
 import './Card.css';
 
-function Card() {
-  return (
-    <div className="Card">
+function Card(props) {
+  const MAX_VALUE = 10;
+  const value = Math.floor(Math.random() * MAX_VALUE) + 1;
 
+  return (
+    <div className="card">
+      <div>Name: {props.englishName}</div>
+      <img className='card-image' src={props.image} alt="" />
+      <div>Description: {props.description}</div>
+      <div className='value' >Value: {value}</div>
     </div>
   );
 }
